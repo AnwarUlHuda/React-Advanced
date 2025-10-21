5c2ecdd372eefe7e94da9eaab1f7719cbd6bd46b
AnwarUlHuda <anwarulhudaam@gmail.com>
2025-10-21 16:27:03 +0530

    changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,13 +10,14 @@ const Cart = () => {
   const total = cartItems.map((item) => (item.card.info.defaultPrice || item.card.info.price) / 100);
   const price = total.reduce((acc,red) => acc+red,0)
   return (
-    <div className='flex flex-col w-1/2 justify-center m-auto items-center'>
+    cartItems.length > 0 ?
+      <div className='flex flex-col w-1/2 justify-center m-auto items-center'>
       <button className='p-2 bg-black text-white rounded-full cursor-pointer' onClick={() => dispatch(clearCart())}>Clear Cart <span className='px-1.5 mx-1 text-red-700 font-bold bg-white rounded-2xl'> X</span></button>
       <ItemList cards={cartItems} cart />
-      {cartItems.length> 0 && <div className="text-right font-semibold w-1/2 p-2 m-2">
+       <div className="text-right font-semibold w-1/2 p-2 m-2">
         <h2>Total : { price}</h2>
-      </div>}
-    </div>
+      </div>
+    </div> : <div className='flex text-lg text-red-600 font-semibold justify-center'>Looks empty here... Please add your favourite items to cart !</div>
   )
 }
 
