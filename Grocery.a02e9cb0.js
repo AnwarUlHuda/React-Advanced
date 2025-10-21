01d311fff30b8a06bcf41ae349dd6375e1df2198
AnwarUlHuda <anwarulhudaam@gmail.com>
2025-10-21 09:21:19 +0530

    Updates

---
+++ Grocery.a02e9cb0.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequiree166;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var t=e("ayMG0");e("acw62");var a=()=>(0,t.jsx)("div",{children:(0,t.jsx)("h1",{style:{textAlign:"center"},children:"Grocery, this is bundled separately, using lazy loading for performance optimization."})})});
+//# sourceMappingURL=Grocery.a02e9cb0.js.map
